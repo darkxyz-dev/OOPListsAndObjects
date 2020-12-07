f864834a8ff1d014ec2fd1ec4b87f60ebbadcbd4
Opilane TTHK <opilane@tthk.ee>
2020-12-07 15:35:26 +0200

    first commit

---
+++ OOPListsAndObjects/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOPListsAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
